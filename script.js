30c881c35b8ffe8bed04da63a92d175c4b771ae0
U-ma-s <yuma.prodia4989@gmail.com>
2022-08-08 21:31:10 +0900

    add my status

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Engineer","Developer", "Blogger", "Designer", "Freelancer","TheAppWizard"],
+        strings: ["Engineer","Developer", "Student"],
         typeSpeed: 100,
         backSpeed: 60,
         cursorChar: ' |',
